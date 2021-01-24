ea7c358e599b9012c5983325b709fb91e26d5525
Michael Johnson <mrj0730@icloud.com>
2021-01-24 04:37:37 -0600

    add css for aircraft images on the airport display

--- components/Question.jsx
+++ components/Question.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
 
 const Question = ({ questionData }) => {
   const { trail, lead } = questionData;
@@ -16,8 +18,41 @@ const Question = ({ questionData }) => {
     return labels[ac.departurePoint];
   }
 
+  function position(ac, acId) {
+    const list = [acId];
+
+    if (ac.runway) {
+      list.push(ac.runway);
+      return list.join(' ');
+    }
+
+    if (ac.departurePoint === 'full length') {
+      list.push('full');
+    } else {
+      list.push(ac.departurePoint);
+    }
+
+    return list.join(' ');
+  }
+
   return (
     <section id='question'>
+      <div className='airportContainer'>
+        <div className='legend'>
+          <div className='legend-lead'>
+            <span className='text'>Lead </span>
+            <FontAwesomeIcon icon={faPlane} /> {lead.name}
+          </div>
+
+          <div className='legend-trail'>
+            <span className='text'>Trail </span>
+            <FontAwesomeIcon icon={faPlane} /> {trail.name}
+          </div>
+        </div>
+        <FontAwesomeIcon icon={faPlane} className={position(lead, 'lead')} />
+        <FontAwesomeIcon icon={faPlane} className={position(trail, 'trail')} />
+        <img src='./airport.svg' alt='Academy Airport' className='airport' />
+      </div>
       <p>{`${trail.name} departing from ${depPoint(trail)} behind a ${lead.name} at ${depPoint(lead)}.`}</p>
     </section>
   );
