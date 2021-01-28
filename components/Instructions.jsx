d958dc97e8cc6f6e7a38f9029ba8308cd80be24c
Michael Johnson <mrj0730@icloud.com>
2021-01-28 15:46:59 -0600

    fix typo

--- components/Instructions.jsx
+++ components/Instructions.jsx
@@ -16,7 +16,7 @@ const Instructions = ({ start, options, setOptions }) => {
         Select the wake turbulence time requirement and if that requirement is waivealbe or not. Use the buttons below
         to enter your answer, then check it with the <FontAwesomeIcon icon={faCheckCircle} className='color-green' />{' '}
         button. Go to the next question with the <FontAwesomeIcon icon={faChevronCircleRight} className='color-white' />{' '}
-        buttons. Return to these instructions by clicking the{' '}
+        button. Return to these instructions by clicking the{' '}
         <FontAwesomeIcon icon={faInfoCircle} className='color-white' />. View the list of aircraft and thier
         characteristics used in the questions with the <FontAwesomeIcon icon={faPlane} className='color-white' /> icon.
       </p>
