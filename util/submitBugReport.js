b6751d889f22284e40d5aaffbe7ddf93422b1b76
Michael Johnson <mrj0730@icloud.com>
2021-11-20 00:22:56 -0600

    add bug report logic, but it wont work as a function call from front end, it needs to be moved to the server side

---
+++ util/submitBugReport.js
@@ -0,0 +1,28 @@
+//todo: this needs to convert to an api endpoint. the env vars arent available to the front end, plus cors
+async function submitBugReport(questionInfo, userMessage) {
+  let xfUser = process.env.XF_API_USER;
+  let xfKey = process.env.XF_API_KEY;
+
+  console.log(xfUser, xfKey);
+  const message = { questionInfo, userMessage };
+  const xfPostThreadParams = { node_id: 2, title: 'Some Title', message: message };
+  const body = new URLSearchParams(xfPostThreadParams);
+
+  const response = await fetch('https://pointsixtyfive.com/dev/api', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'XF-Api-Key': xfKey,
+      'XF-Api-User': xfUser,
+    },
+    body: body,
+  });
+
+  return response.json();
+}
+
+export default submitBugReport;
+
+//for encoding body as form urlencoded
+//https://stackoverflow.com/questions/35325370/how-do-i-post-a-x-www-form-urlencoded-request-using-fetch
